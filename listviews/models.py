b6eb149a129690d3d9a647d5e2778ebed10e6763
pizzaubu <pongpanot.wa.63@ubu.ac.th>
2023-12-01 14:50:37 +0700

    create class User

--- listviews/models.py
+++ listviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)  #สามารถใช้ Django's built-in User model ได้
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
