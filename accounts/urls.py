7b4b68d18a5424682026ac9e19bd2f256b24cee3
pizzaubu <pongpanot.wa.63@ubu.ac.th>
2023-12-02 15:23:06 +0700

    remove home in account urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.user_login, name='user_login'),
     path('register/', views.user_register, name='user_register'),
-    path('home/', views.home, name='home'),  # URL สำหรับหน้าหลักหลังจาก login/register
+    
 ]
