cb8ca771595d334144b93c4578dfd45078d43991
pizzaubu <pongpanot.wa.63@ubu.ac.th>
2024-01-08 16:09:30 +0700

    add mediea root

--- MMR/urls.py
+++ MMR/urls.py
@@ -25,3 +25,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
 ]
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- listviews/models.py
+++ listviews/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 
-
+class Report(models.Model):
+    header = models.CharField(max_length=20)
+    report_id = models.CharField(max_length=20)
+    file = models.FileField(upload_to='reports/')
