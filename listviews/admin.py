997dbefb8a945e82c4797e5a7ab4ae29a3b5cc4e
pizzaubu <pongpanot.wa.63@ubu.ac.th>
2024-01-11 15:37:20 +0700

    fix admins.py in listviews

--- listviews/admin.py
+++ listviews/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Report
+from .forms import ReportForm
 
-# Register your models here.
+class ReportAdmin(admin.ModelAdmin):
+    form = ReportForm
+    list_display = ('header', 'report_id', 'file')
+    search_fields = ('header', 'report_id')
+
+    def save_model(self, request, obj, form, change):
+        if change:  # การแก้ไขรายงาน
+            # ลบไฟล์เก่าถ้ามีการอัปโหลดไฟล์ใหม่
+            if 'file' in request.FILES:
+                if obj.file:
+                    obj.file.delete()
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Report, ReportAdmin)
