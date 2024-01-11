7e81900eefafc03cbf04668b71e7828d9fae4e61
pizzaubu <pongpanot.wa.63@ubu.ac.th>
2024-01-11 15:15:24 +0700

    edit report template and function

--- listviews/urls.py
+++ listviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import upload_report
+from .views import upload_report,edit_report
 
 urlpatterns = [
     path('upload/', upload_report, name='upload_report'),
+    path('report/edit/<int:report_id>/', edit_report, name='edit_report'),
     # โค้ดอื่นๆ...
 ]
--- listviews/views.py
+++ listviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import ReportForm
 
 def upload_report(request):
@@ -10,3 +10,19 @@ def upload_report(request):
     else:
         form = ReportForm()
     return render(request, 'upload_report.html', {'form': form})
+
+
+def edit_report(request, report_id):
+    report = get_object_or_404(Report, pk=report_id)
+    if request.method == 'POST':
+        form = ReportForm(request.POST, request.FILES, instance=report)
+        if form.is_valid():
+            # ลบไฟล์เก่าถ้ามีการอัปโหลดไฟล์ใหม่
+            if 'file' in request.FILES:
+                if report.file:
+                    report.file.delete()
+            form.save()
+            return redirect('some_view_to_redirect_after_editing')
+    else:
+        form = ReportForm(instance=report)
+    return render(request, 'edit_report.html', {'form': form, 'report': report})
